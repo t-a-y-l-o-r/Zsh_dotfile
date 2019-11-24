a8034840a8ba06ef02c3ff8c55943de082a0ecd2
Taylor <taylorjcochran@hotmail.com>
2019-11-24 09:11:55 -0800

    Added dotfile

---
+++ zshrc.zsh
@@ -0,0 +1,71 @@
+# Enable colors and change prompt:
+autoload -U colors && colors
+
+PS1="%B%{$fg[green]%}༇ %{$reset_color%}%b "
+
+# History in cache directory:
+HISTSIZE=10000
+SAVEHIST=10000
+HISTFILE=~/.cache/zsh/history
+
+# Basic auto/tab complete:
+autoload -U compinit
+zstyle ':completion:*' menu select
+zmodload zsh/complist
+compinit
+_comp_options+=(globdots)		# Include hidden files.
+
+# vi mode
+bindkey -v
+export KEYTIMEOUT=1
+
+# Change cursor shape for different vi modes.
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] ||
+     [[ $1 = 'block' ]]; then
+    echo -ne '\e[1 q'
+  elif [[ ${KEYMAP} == main ]] ||
+       [[ ${KEYMAP} == viins ]] ||
+       [[ ${KEYMAP} = '' ]] ||
+       [[ $1 = 'beam' ]]; then
+    echo -ne '\e[5 q'
+  fi
+}
+zle -N zle-keymap-select
+zle-line-init() {
+    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+    echo -ne "\e[5 q"
+}
+zle -N zle-line-init
+echo -ne '\e[5 q' # Use beam shape cursor on startup.
+preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+
+# Changes the dir if one is passed
+# always prints the cwd and contents :)
+function lwd(){
+  clear
+  if [ ! -z "$@" ]; then
+      cd "$@"
+  fi
+  pwd
+  ls
+}
+
+# check current ip
+alias ip="curl icanhazip.com"
+# better than restarting
+alias rezsh="source ~/.zshrc"
+
+
+# Edit line in vim with ctrl-e:
+autoload edit-command-line; zle -N edit-command-line
+bindkey '^e' edit-command-line
+
+# Load aliases and shortcuts if existent.
+[ -f "$HOME/.config/shortcutrc" ] && source "$HOME/.config/shortcutrc"
+[ -f "$HOME/.config/aliasrc" ] && source "$HOME/.config/aliasrc"
+
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# Load zsh-syntax-highlighting; should be last.
+source /Users/taylorcochran/.zsh_plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 2>/dev/null
